92e1cd1a36e18e2772d69244f8c480bf62a5d00f
Teddy Okatch <okatch.tedbicah@gmail.com>
2021-10-17 16:39:36 +0300

    Added crude navbar and drawer

---
+++ src/components/shared/NavBar.vue
@@ -0,0 +1,55 @@
+<template>
+  <div
+    class="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box"
+  >
+    <div class="flex-none">
+      <button class="btn btn-square btn-ghost">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          class="inline-block w-6 h-6 stroke-current"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h16M4 18h16"
+          ></path>
+        </svg>
+      </button>
+    </div>
+    <div class="flex-1 px-2 mx-2">
+      <span class="text-lg font-bold"> Hi {{ username }} </span>
+    </div>
+    <div class="flex-none">
+      <button class="btn btn-square btn-ghost">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          class="inline-block w-6 h-6 stroke-current"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+          ></path>
+        </svg>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "NavBar",
+  computed: {
+    ...mapGetters(["username"]),
+  },
+};
+</script>
+
+<style></style>

ea55f334c768f58c40aca26f40ca0109d50b5c6e
Teddy Okatch <okatch.tedbicah@gmail.com>
2021-10-24 23:27:57 +0300

    Showing school details

--- src/pages/auth/MyAccount.vue
+++ src/pages/auth/MyAccount.vue
@@ -2,20 +2,39 @@
   <Base01>
     <h1 class="text-2xl font-normal mb-2 px-2">My Account</h1>
     <div class="btn-group">
-      <router-link to="/reg-school" class="btn btn-info">
+      <router-link to="/reg-school" class="btn btn-info" v-if="!school_id">
         Register School
       </router-link>
+    </div>
+
+    {{ school_id }}
+    {{ school }}
+
+    <div class="card bg-base-200 bg-opacity-60" v-if="school_id">
+      <div class="card-body">
+        <table class="table w-full table-zebra">
+          <tbody>
+            <tr v-for="item in school" :key="item.key">
+              <th>{{ item.key }}</th>
+              <td>{{ item.value }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </Base01>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 import Base01 from "../../components/layouts/Base01.vue";
 export default {
   name: "MyAccount",
   components: {
     Base01,
+  },
+  computed: {
+    ...mapGetters({ school_id: "schools/id", school: "schools/school" }),
   },
   methods: {
     ...mapActions({
--- src/utils/state/modules/schools.js
+++ src/utils/state/modules/schools.js
@@ -23,6 +23,13 @@ const getters = {
   created_at: (state) => state.created_at,
   updated_at: (state) => state.updated_at,
   errors: (state) => state.errors,
+  school: (state) => {
+    let schoolDetails = [];
+    for (let [key, value] of Object.entries(state)) {
+      schoolDetails.push({ key: key, value: value });
+    }
+    return schoolDetails;
+  },
 };
 
 const actions = {
@@ -57,12 +64,27 @@ const actions = {
     await axios
       .get(`/schools/${userId}/`)
       .then((response) => {
-        console.log(response);
+        console.log("getSchool", response);
+        commit("setSchool", response.data);
       })
       .catch((error) => {
         if (error.response && error.response.data) {
+          console.log(error.response.status);
           console.log(error.response.data);
-          commit("setErrors", error.response.data);
+
+          if (error.response.status === 404) {
+            commit("setSchool", {
+              id: "",
+              name: "",
+              address: "",
+              location: "",
+              country: "",
+              user_id: -1,
+              created_at: "",
+              updated_at: "",
+              errors: [],
+            });
+          }
         } else {
           console.log(error);
         }
@@ -79,6 +101,16 @@ const mutations = {
         state.errors.push({ key: key, value: value[i] });
       }
     }
+  },
+  setSchool: (state, details) => {
+    state.id = details.id;
+    state.name = details.name;
+    state.address = details.address;
+    state.location = details.location;
+    state.country = details.country;
+    state.user_id = details.user_id;
+    state.created_at = details.created_at;
+    state.updated_at = details.updated_at;
   },
 };
 

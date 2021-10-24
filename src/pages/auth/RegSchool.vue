4a40a8b5c247c9e5e5ef0924a98673062dbc22b4
Teddy Okatch <okatch.tedbicah@gmail.com>
2021-10-24 16:16:45 +0300

    ready to submit

--- src/pages/auth/RegSchool.vue
+++ src/pages/auth/RegSchool.vue
@@ -2,20 +2,42 @@
   <Base01>
     <h1 class="text-2xl font-normal mb-2 px-2">Register School</h1>
     <div class="p-10 card bg-base-200 bg-opacity-60">
-      <div class="w-auto justify-around">
+      <form class="w-auto justify-around" @submit="submit">
         <div class="w-full flex space-x-5">
           <div class="form-control w-6/12">
             <label class="label">
               <span class="label-text">Name</span>
             </label>
-            <input type="text" placeholder="name" class="input" />
+            <input
+              type="text"
+              placeholder="name"
+              class="input"
+              :class="{ 'input-error input-bordered': nameValidation }"
+              v-model="name"
+            />
+            <label class="label" v-if="nameValidation">
+              <span class="label-text-alt text-error">
+                {{ nameValidation }}
+              </span>
+            </label>
           </div>
 
           <div class="form-control w-6/12">
             <label class="label">
               <span class="label-text">Address</span>
             </label>
-            <input type="text" placeholder="address" class="input" />
+            <input
+              type="text"
+              placeholder="address"
+              class="input"
+              :class="{ 'input-error input-bordered': addressValidation }"
+              v-model="address"
+            />
+            <label class="label" v-if="addressValidation">
+              <span class="label-text-alt text-error">
+                {{ addressValidation }}
+              </span>
+            </label>
           </div>
         </div>
 
@@ -24,29 +46,91 @@
             <label class="label">
               <span class="label-text">Location</span>
             </label>
-            <input type="text" placeholder="location" class="input" />
+            <input
+              type="text"
+              placeholder="location"
+              class="input"
+              :class="{ 'input-error input-bordered': locationValidation }"
+              v-model="location"
+            />
+            <label class="label" v-if="locationValidation">
+              <span class="label-text-alt text-error">
+                {{ locationValidation }}
+              </span>
+            </label>
           </div>
 
           <div class="form-control w-6/12">
             <label class="label">
               <span class="label-text">Country</span>
             </label>
-            <input type="text" placeholder="country" class="input" />
+
+            <select
+              class="select"
+              :class="{ 'select-error select-bordered': countryValidation }"
+              v-model="country"
+            >
+              <option value="" disabled="disabled" selected="selected">
+                country
+              </option>
+              <option v-for="ct in countries" :key="ct.code" :value="ct.code">
+                {{ ct.name }}
+              </option>
+            </select>
+
+            <label class="label" v-if="countryValidation">
+              <span class="label-text-alt text-error">
+                {{ countryValidation }}
+              </span>
+            </label>
           </div>
         </div>
 
-        <button class="btn btn-primary mt-4">Sign In</button>
-      </div>
+        <button class="btn btn-primary mt-4">Register</button>
+      </form>
     </div>
   </Base01>
 </template>
 
 <script>
 import Base01 from "../../components/layouts/Base01.vue";
+import countries from "../../utils/data/countries";
 export default {
   name: "RegSchool",
   components: {
     Base01,
+  },
+  data() {
+    return {
+      countries: countries,
+      name: "",
+      address: "",
+      location: "",
+      country: "",
+      nameValidation: "",
+      addressValidation: "",
+      locationValidation: "",
+      countryValidation: "",
+    };
+  },
+  methods: {
+    submit(e) {
+      e.preventDefault();
+      const fillThis = "This field should not be left empty";
+      this.nameValidation = this.name === "" ? fillThis : "";
+      this.addressValidation = this.address === "" ? fillThis : "";
+      this.locationValidation = this.location === "" ? fillThis : "";
+      this.countryValidation = this.country === "" ? fillThis : "";
+
+      if (
+        this.nameValidation !== "" ||
+        this.addressValidation !== "" ||
+        this.locationValidation !== "" ||
+        this.countryValidation !== ""
+      ) {
+        return;
+      }
+    },
   },
 };
 </script>

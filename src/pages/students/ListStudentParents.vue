40a0b843006a79643840d283f873ab409523303a
Teddy Okatch <okatch.tedbicah@gmail.com>
2021-11-11 23:30:01 +0300

    Displaying student parents

--- src/pages/students/ListStudentParents.vue
+++ src/pages/students/ListStudentParents.vue
@@ -1,32 +1,54 @@
 <template>
   <Base01>
     <h1 class="text-2xl font-normal mb-2 px-2">Your Parents</h1>
-    {{ studentParentList }}
 
     <div class="card bg-base-200 bg-opacity-60 mb-4">
       <div class="card-body">
         <div class="flex justify-between items-end mb-2">
           <div class="font-bold">Parents</div>
-          <router-link to="edit-parent" class="btn btn-sm btn-info">
-            <i class="fa fa-pencil mr-2 text-xs"></i>
-            Edit
-          </router-link>
+          <div class="flex justify-end space-x-2">
+            <router-link to="edit-parent" class="btn btn-sm btn-info">
+              <i class="fa fa-plus mr-2 text-xs"></i>
+              Add
+            </router-link>
+          </div>
         </div>
         <table class="table w-full table-zebra">
           <thead>
             <th>#</th>
             <th>Student Id</th>
             <th>Parent Id</th>
+            <th>Parent Name</th>
             <th>Created At</th>
             <th>Updated At</th>
+            <th>Actions</th>
           </thead>
           <tbody>
             <tr v-for="item in studentParentList" :key="item.id">
               <td>{{ item.id }}</td>
               <td>{{ item.student_id }}</td>
-              <td>{{ item.parent_id }}</td>
+              <td>{{ item.parent_id.id }}</td>
+              <td>
+                {{ item.parent_id.first_name }} {{ item.parent_id.last_name }}
+              </td>
               <td>{{ item.created_at }}</td>
               <td>{{ item.updated_at }}</td>
+              <td>
+                <div class="space-x-2">
+                  <router-link
+                    to="edit-parent"
+                    class="btn btn-circle btn-sm btn-info"
+                  >
+                    <i class="fa fa-pencil text-xs"></i>
+                  </router-link>
+                  <router-link
+                    to="edit-parent"
+                    class="btn btn-circle btn-sm btn-error"
+                  >
+                    <i class="fa fa-trash-alt text-xs"></i>
+                  </router-link>
+                </div>
+              </td>
             </tr>
           </tbody>
         </table>

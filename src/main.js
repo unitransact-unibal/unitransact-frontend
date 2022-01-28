7ed00166c1930c2c4020f524f7cbad30af930534
Sospeter Birir <sospeterbirir1@gmail.com>
2022-01-29 00:07:15 +0300

    Added correct address

--- src/main.js
+++ src/main.js
@@ -6,6 +6,6 @@ import axios from "axios";
 import App from "./App.vue";
 import "./assets/styles.css";
 
-axios.defaults.baseURL = "http://localhost:8000/api/v1";
+axios.defaults.baseURL = "https://unibal-backend.herokuapp.com/api/v1";
 
 createApp(App).use(router).use(state).mount("#app");
